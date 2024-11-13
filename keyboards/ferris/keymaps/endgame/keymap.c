6a19e5638760e18d475a69606e20c2e90d68a21f
SanderMar <sander.marinus@student.uantwerpen.be>
2024-11-14 00:46:29 +0100

    Setup basic ferris keymap

--- keyboards/ferris/keymaps/endgame/keymap.c
+++ keyboards/ferris/keymaps/endgame/keymap.c
@@ -16,41 +16,67 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-#define KC_CTSC RCTL_T(KC_SCLN)
-#define KC_CTLA LCTL_T(KC_A)
-#define KC_LSHZ LSFT_T(KC_Z)
-#define KC_RLSH RSFT_T(KC_SLSH)
-#define KC_SPM2 LT(2, KC_SPC)
-#define KC_BSM1 LT(1, KC_BSPC)
-#define KC_GUTA GUI_T(KC_TAB)
-#define KC_CLGV CTL_T(KC_GRV)
+#define KC_LALA LALT_T(KC_A)
+#define KC_LGUR LGUI_T(KC_R)
+#define KC_LCTS LCTL_T(KC_S)
+#define KC_LSFT LSFT_T(KC_T)
+#define KC_RSFN RSFT_T(KC_N)
+#define KC_RCTE RCTL_T(KC_E)
+#define KC_RGUI RGUI_T(KC_I)
+#define KC_RALO RALT_T(KC_O)
+
+#define KC_SFEQ LSFT_T(KC_EQL)
+
+#define KC_TBLN LT(_NUMS, KC_TAB)
+#define KC_ESLS NT(_SYMS, KC_ESC)
+
+#define KC_GUPL LGUI_T(KC_MPLY)
+#define KC_CTPR LCTL_T(KC_MPRV)
+#define KC_SFNX LSFT_T(KC_MNXT)
+
+#define KC_BACK LCTL(LSFT(KC_TAB))
+#define KC_FWD LCTL(KC_TAB)
+
+#define OSM_CTL OSM(MOD_LCTL)
+#define OSM_GUI OSM(MOD_LGUI)
+#define OSM_ALT OSM(MOD_LALT)
+
+#define OSL_NAV OSL(_NAV)
+
+// clang-format off
+enum layers {
+    _HOME,
+    _NUMS,
+    _SYMS,
+    _NAV
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
-                                    KC_CLGV, KC_BSM1, KC_SPM2, KC_GUTA
+  [_HOME] = LAYOUT(
+    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,            KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
+    KC_LALA, KC_LGUR, KC_LCTS, KC_LSFT, KC_G,            KC_M,    KC_RSFN, KC_RCTE, KC_RGUI, KC_RALO,
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,            KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RLSH,
+                                  KC_TBLN, KC_BSPC, KC_SPC, KC_ESLS
   ),
 
-  [1] = LAYOUT(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_NUMS] = LAYOUT(
+    KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PPLS,         KC_F6,   KC_SFEQ, OSM_CTL, OSM_GUI, OSM_ALT,
+    KC_PSLS, KC_P4,   KC_P5,   KC_P6,   KC_PDOT,         KC_F5,   KC_F4,   KC_F3,   KC_F2,   KC_F1,
+                                    _______, _______, _______, OSL_NAV
   ),
 
-  [2] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, QK_BOOT,
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_SYMS] = LAYOUT(
+    _______, _______, KC_COLN, KC_LT,   KC_GT,         KC_PLUS, KC_DQT,  KC_QUOT, KC_BSLS, _______,
+    KC_CIRC, KC_LCBR, KC_LBRC, KC_LPRN, KC_AMPR,         KC_HASH, KC_MINS, KC_UNDS, KC_SLSH, KC_DLR,
+    KC_EXLM, KC_RCBR, KC_RBRC, KC_RPRN, KC_AT,           KC_PERC, KC_TILD, KC_GRV,  KC_PIPE, KC_ASTR,
+                                    OSL_NAV, _______, _______, _______
   ),
 
-  [3] = LAYOUT(
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  [_NAV] = LAYOUT(
+    XXXXXXX,  XXXXXXX, XXXXXXX, KC_VOLU, XXXXXXX,         KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_PSCR,
+    KC_LALT,  KC_GUPL, KC_CTPR, KC_SFNX, KC_MUTE,         KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_DEL,
+    XXXXXXX,  XXXXXXX, KC_CAPS, KC_VOLD, XXXXXXX,         KC_UP,   KC_LEFT, XXXXXXX, XXXXXXX, XXXXXXX,
+                                    XXXXXXX, KC_BACK, KC_FWD,  XXXXXXX
   ),
 };
