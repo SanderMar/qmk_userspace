37a86f00f16dfdf7dfeff14a1ebb1378d738bb14
SanderMar <sander.marinus@student.uantwerpen.be>
2026-03-14 17:12:58 +0100

    Add chordal hold handedness definition

--- keyboards/ferris/keymaps/endgame/keymap.c
+++ keyboards/ferris/keymaps/endgame/keymap.c
@@ -148,3 +148,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,  _______
   )
 };
+
+/******
+ * Define chordal hold handedness
+ ******/
+
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R',
+                       '*', '*', '*', '*'
+    );
